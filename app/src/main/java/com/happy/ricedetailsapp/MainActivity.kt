bb94ed2fd05bcb4aabc1a18cc2f3bbb0e1e55c87
Shubham Jaydeokar <Shubham.Jaydeokar@ril.com>
2020-05-06 22:59:46 +0530

    initial commit of full repo

---
+++ app/src/main/java/com/happy/ricedetailsapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.happy.ricedetailsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
