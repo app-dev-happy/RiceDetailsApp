3896e648354cd6b7eb4cf7c2f4817678e3f4f9fc
lakhan.vijayvargiya <lakhan.vijayvargiya@ril.com>
2020-06-06 15:42:38 +0530

    dashboard plot dummy

--- app/src/main/java/com/happy/ricedetailsapp/adapter/DashboardMainRecyclerAdapter.kt
+++ app/src/main/java/com/happy/ricedetailsapp/adapter/DashboardMainRecyclerAdapter.kt
@@ -28,6 +28,7 @@ class DashboardMainRecyclerAdapter(val mContext: Context?,val dashboardMainConte
         holder.mBinding.recycler.layoutManager = LinearLayoutManager(mContext, LinearLayoutManager.HORIZONTAL,false)
         holder.mBinding.recycler.adapter = mAdapter
 
+
     }
 
     inner class DashboardMainRecyclerAdapterViewHolder(private val mContext: Context?,  val mBinding: LayoutRiceMainVarietyBinding):androidx.recyclerview.widget.RecyclerView.ViewHolder(mBinding!!.root)
