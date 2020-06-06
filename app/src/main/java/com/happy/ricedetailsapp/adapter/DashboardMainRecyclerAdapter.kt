dd87636a4f2c64cb464a289a23d229608c2436d8
lakhan.vijayvargiya <lakhan.vijayvargiya@ril.com>
2020-06-06 15:06:33 +0530

    dashboard plot

--- app/src/main/java/com/happy/ricedetailsapp/adapter/DashboardMainRecyclerAdapter.kt
+++ app/src/main/java/com/happy/ricedetailsapp/adapter/DashboardMainRecyclerAdapter.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.happy.ricedetailsapp.R
 import com.happy.ricedetailsapp.databinding.LayoutRiceMainVarietyBinding
 import com.happy.ricedetailsapp.pojo.DashboardMainContent
 
 class DashboardMainRecyclerAdapter(val mContext: Context?,val dashboardMainContent:ArrayList<DashboardMainContent>) : androidx.recyclerview.widget.RecyclerView.Adapter<androidx.recyclerview.widget.RecyclerView.ViewHolder>() {
+    lateinit var mAdapter:RiceCatagoryItemAdapter
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         var binding:LayoutRiceMainVarietyBinding=  DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.layout_rice_main_variety,parent,false)
         return DashboardMainRecyclerAdapterViewHolder(mContext,binding)
@@ -22,9 +24,11 @@ class DashboardMainRecyclerAdapter(val mContext: Context?,val dashboardMainConte
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
        val item = dashboardMainContent.get(position)
         (holder as DashboardMainRecyclerAdapterViewHolder).mBinding.languageItemText.text = item.title
+        mAdapter = RiceCatagoryItemAdapter(mContext,item.varietyItems)
+        holder.mBinding.recycler.layoutManager = LinearLayoutManager(mContext, LinearLayoutManager.HORIZONTAL,false)
+        holder.mBinding.recycler.adapter = mAdapter
     }
 
     inner class DashboardMainRecyclerAdapterViewHolder(private val mContext: Context?,  val mBinding: LayoutRiceMainVarietyBinding):androidx.recyclerview.widget.RecyclerView.ViewHolder(mBinding!!.root)
-
 
 }
---
+++ app/src/main/java/com/happy/ricedetailsapp/adapter/RiceCatagoryItemAdapter.kt
@@ -0,0 +1,43 @@
+package com.happy.ricedetailsapp.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.happy.ricedetailsapp.R
+import com.happy.ricedetailsapp.databinding.RiceCategoryItemLayoutBinding
+import com.happy.ricedetailsapp.pojo.VarietyItem
+
+class RiceCatagoryItemAdapter(val mContext: Context?, val riceVarietyList:ArrayList<VarietyItem>) : androidx.recyclerview.widget.RecyclerView.Adapter<androidx.recyclerview.widget.RecyclerView.ViewHolder>()  {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        var binding:RiceCategoryItemLayoutBinding=  DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.rice_category_item_layout,parent,false)
+        return RiceCatagoryItemAdapterViewHolder(mContext,binding)
+    }
+
+    override fun getItemCount(): Int {
+        return riceVarietyList.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val item = riceVarietyList.get(position)
+        val marg_10 = mContext?.resources!!.getDimensionPixelSize(R.dimen.text_size_10sp)
+        val top=mContext?.resources!!.getDimensionPixelSize(R.dimen.text_size_10sp)
+        val marg_16=mContext?.resources!!.getDimensionPixelSize(R.dimen.text_jioverify_success)
+        val bottom=mContext?.resources!!.getDimensionPixelSize(R.dimen.text_size_10sp)
+        val layoutParams = (holder as RiceCatagoryItemAdapterViewHolder).mBinding.root.layoutParams as ViewGroup.MarginLayoutParams
+
+        if(position == 0){
+            layoutParams.setMargins(marg_16, top, 0, bottom)
+        }else if(position == (riceVarietyList.size-1) ) {
+            layoutParams.setMargins(marg_10, top, marg_16, bottom)
+        }
+        else{
+            layoutParams.setMargins(marg_10, top, 0, bottom)
+        }
+        (holder as RiceCatagoryItemAdapterViewHolder).mBinding.riceItemSubtext.text = item.title
+    }
+
+    inner class RiceCatagoryItemAdapterViewHolder(private val mContext: Context?,  val mBinding: RiceCategoryItemLayoutBinding):androidx.recyclerview.widget.RecyclerView.ViewHolder(mBinding!!.root)
+
+}
