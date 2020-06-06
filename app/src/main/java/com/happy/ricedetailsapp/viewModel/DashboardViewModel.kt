f81b619f4876f87aac0883ebda029ceb8f430536
lakhan.vijayvargiya <lakhan.vijayvargiya@ril.com>
2020-06-06 15:51:51 +0530

    api change

--- app/src/main/java/com/happy/ricedetailsapp/viewModel/DashboardViewModel.kt
+++ app/src/main/java/com/happy/ricedetailsapp/viewModel/DashboardViewModel.kt
@@ -12,7 +12,7 @@ class DashboardViewModel: ViewModel() {
 
     fun readDashboardFile(context:Context): LiveData<String> {
         var mDashboardFileLiveData: MutableLiveData<String> = MutableLiveData<String>()
-        val url = "https://ricedetailsapp.free.beeceptor.com/my/api/path"
+        val url = "https://webhook.site/1bf0b1b8-d26a-4b68-b17f-f44cf1414768"
         try{
         CoroutineScope(Dispatchers.IO).launch {
             val job = async { FileDataCoroutines().getDataFromServer(url, context) }
